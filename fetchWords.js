72da48941fc831bbaf109c7bc188e9e70a98c8fc
Mikolaj Cygert <miki@comsite.com.pl>
2020-07-06 20:41:56 +0200

    Make fetch working

--- fetchWords.js
+++ fetchWords.js
@@ -1,4 +1,11 @@
-let fruits = fetch("https://api.predic8.de:443/shop/products/").then(
-  (data) => data.json
-);
-fruits.then((items) => console.log(items));
+function getData() {
+  return fetch("https://api.predic8.de:443/shop/products/")
+    .then((response) => response.json())
+    .then((json) => json.products)
+    .catch(() => console.log("Something went wrong"));
+}
+
+let fruits = null;
+getData().then((json) => (fruits = console.log(json.map((name) => name.name))));
+// fruits = fruits.map((jsonReponse) => jsonReponse.fruits);
+console.log(fruits);
