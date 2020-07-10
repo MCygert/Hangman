2fc224bb5d9c8a5a59342e18e29e8899b78db937
Mikolaj Cygert <miki@comsite.com.pl>
2020-07-11 01:27:15 +0200

    Add creating input boxes after the page is loaded Not working because the words are not fetched yet.

--- fetchWords.js
+++ fetchWords.js
@@ -1,21 +1,22 @@
 function getData() {
-  return fetch("https://api.predic8.de:443/shop/products/")
-    .then((response) => response.json())
-    .then((json) => json.products)
-    .catch(() => console.log("Something went wrong"));
+    return fetch("https://api.predic8.de:443/shop/products/")
+        .then((response) => response.json())
+        .then((json) => json.products)
+        .catch(() => console.log("Something went wrong"));
 }
 
 let fruits = null;
 getData().then((json) => (fruits = json.map((name) => name.name)));
-
 function createInputBoxes() {
   let container = document.getElementById("inputBoxes");
   container.innerHTML = "";
   let randomWord = fruits[Math.floor(Math.random() * fruits.length)];
   console.log(randomWord);
+    for (let index = 0; index < randomWord.length; index++) {
 
-  for (let index = 0; index < randomWord.length; index++) {
-    let input = document.createElement("input");
-    container.appendChild(input);
-  }
+      let input = document.createElement("input");
+      container.appendChild(input);
+    }
+    return randomWord;
 }
+document.onload(createInputBoxes());
