a2b6f6dda2fb5564aef0e3e1f0314933dbb38799
Mikolaj Cygert <miki@comsite.com.pl>
2020-07-11 12:04:16 +0200

    Fix fetch and add logic to event

--- fetchWords.js
+++ fetchWords.js
@@ -1,27 +1,38 @@
+let fruits = null;
+let randomWord;
+let container = null;
+
+
 function getData() {
-    return fetch("https://api.predic8.de:443/shop/products/")
+
+  return fetch("https://api.predic8.de:443/shop/products/")
         .then((response) => response.json())
         .then((json) => json.products)
         .catch(() => console.log("Something went wrong"));
 }
 
-let fruits = null;
-getData().then((json) => (fruits = json.map((name) => name.name)));
+getData().then((json) => (fruits = json.map((name) => name.name))).finally( () => createInputBoxes());
+function validate(target){
 
-function renderInputBoxes(randomWord, container) {
+}
+function renderInputBoxes() {
+
   for (let index = 0; index < randomWord.length; index++) {
-
     let input = document.createElement("input");
+    input.indexLetter = index;
+    input.addEventListener("change", (event) => {
+      console.log(event.target.indexLetter);
+      event.target.value;
+
+    })
     container.appendChild(input);
   }
 }
-
 function createInputBoxes() {
-  let container = document.getElementById("inputBoxes");
+  container = document.getElementById("inputBoxes");
   container.innerHTML = "";
-  let randomWord = fruits[Math.floor(Math.random() * fruits.length)];
+  randomWord = fruits[Math.floor(Math.random() * fruits.length)];
   console.log(randomWord);
-  renderInputBoxes(randomWord, container);
+  renderInputBoxes();
   return randomWord;
 }
-document.onload(createInputBoxes());
