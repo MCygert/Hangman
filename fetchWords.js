8afde9e5d0ab68a3470413ca581db7ba96e4645e
Mikolaj Cygert <miki@comsite.com.pl>
2020-07-14 19:06:48 +0200

    Add, adding text to span still needs seperating between letters.

--- fetchWords.js
+++ fetchWords.js
@@ -1,74 +1,100 @@
 let fruits = null;
 let randomWord;
-let container = null;
+let inputContainer = null;
+let spanContainer = null;
 let tries = 9;
 
 
 function getData() {
 
-  return fetch("https://api.predic8.de:443/shop/products/")
+    return fetch("https://api.predic8.de:443/shop/products/")
         .then((response) => response.json())
         .then((json) => json.products)
         .catch(() => console.log("Something went wrong"));
 }
 
-getData().then((json) => (fruits = json.map((name) => name.name))).finally( () => createInputBoxes());
+getData().then((json) => (fruits = json.map((name) => name.name))).finally(() => {
+    createInputBoxes();
+    createSpanPlaces;
+});
 
-function updateTries() {
-  tries--;
-  triesNode.nodeValue = tries;
+function updateTriesCount() {
+    tries--;
+    triesNode.nodeValue = tries;
 }
 
-function validate(target){
-  if (target.value === randomWord[target.indexLetter]) {
-    target.className = '';
-    target.classList.add("correctAnswer");
-    target.disabled = true;
-    console.log(target);
-  } else if (randomWord.includes(target.value)) {
-    target.className = '';
-    target.classList.add("badPositionAnswer");
-    updateTries();
-  } else {
-    target.className = '';
-    target.classList.add("incorrectAnswer");
-    updateTries();
-    console.log(tries);
-  }
+function validate(target) {
+    if (target.value === randomWord[target.indexLetter]) {
+        target.className = '';
+        target.classList.add("correctAnswer");
+        target.disabled = true;
+        console.log(target);
+    } else if (randomWord.includes(target.value)) {
+        target.className = '';
+        target.classList.add("badPositionAnswer");
+        updateTriesCount();
+    } else {
+        target.className = '';
+        target.classList.add("incorrectAnswer");
+        updateTriesCount();
+        console.log(tries);
+    }
 
 }
 
-function checkIfCorrectLetter(target) {
-  const alphabeticLetters = /^[A-Za-z]+$/;
-  if (!target.value.match(alphabeticLetters)) {
-    target.value = "";
-  }
+function checkIfAlphabetLetter(target) {
+    const alphabeticLetters = /^[A-Za-z]+$/;
+    if (!target.value.match(alphabeticLetters)) {
+        target.value = "";
+    }
+}
+
+function addCorrectLetterToSpan(target) {
+    if (target.className === 'correctAnswer') {
+        let spanBox = document.getElementById("guessedLetters").children;
+        for (let spanBoxElement of spanBox) {
+            console.log(spanBoxElement);
+            if (spanBoxElement.indexLetter === target.indexLetter) {
+                console.log(spanBoxElement.indexLetter)
+                spanBoxElement.innerText = target.value;
+            }
+        }
+    }
 }
 
 function renderInputBoxes() {
 
-  for (let index = 0; index < randomWord.length; index++) {
-    let input = document.createElement("input");
-    input.indexLetter = index;
-    input.addEventListener("change", (event) => {
-      console.log(event.target.indexLetter);
-      console.log(event.target.value);
-      checkIfCorrectLetter(event.target)
-      validate(event.target);
-
-    })
-    container.appendChild(input);
-  }
+    for (let index = 0; index < randomWord.length; index++) {
+        let input = document.createElement("input");
+        input.indexLetter = index;
+        input.addEventListener("change", (event) => {
+            checkIfAlphabetLetter(event.target)
+            validate(event.target);
+            addCorrectLetterToSpan(event.target)
+        })
+        inputContainer.appendChild(input);
+    }
 }
+
 function createInputBoxes() {
-  tries = 9;
-  triesNode.nodeValue = tries.toString();
-  container = document.getElementById("inputBoxes");
-  container.innerHTML = "";
-  randomWord = fruits[Math.floor(Math.random() * fruits.length)];
-  console.log(randomWord);
-  renderInputBoxes();
-  return randomWord;
+    tries = 9;
+    triesNode.nodeValue = tries.toString();
+    inputContainer = document.getElementById("inputBoxes");
+    inputContainer.innerHTML = "";
+    randomWord = fruits[Math.floor(Math.random() * fruits.length)];
+    console.log(randomWord);
+    renderInputBoxes();
+    createSpanPlaces()
+}
+
+function createSpanPlaces() {
+    spanContainer = document.getElementById("guessedLetters")
+    spanContainer.innerHTML = "";
+    for (let i = 0; i < randomWord.length; i++) {
+        let input = document.createElement("span")
+        input.indexLetter = i;
+        spanContainer.appendChild(input)
+    }
 }
 
 let triesNode = document.createTextNode(tries.toString());
